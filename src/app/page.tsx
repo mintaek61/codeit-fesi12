62aa64bb90c7e1b3b7e098d70d3c66fe82a5de27
mintaek <omt610@gmail.com>
2025-11-11 09:08:30 +0900

    utility 클래스 실습 문제2: 카드 만들기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,27 @@
+// src/app/page.tsx
+
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Default
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900"
-      >
-        Alternative
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Dark
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900"
-      >
-        Light
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-green-700 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Green
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Red
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-yellow-400 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Yellow
-      </button>
-      <button
-        type="button"
-        className="mr-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white"
-      >
-        Purple
-      </button>
+      <div className="max-w-sm overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
+        <img
+          src="https://images.unsplash.com/photo-1602080858428-57174f9431cf?q=80&w=2151&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            2024년 기술 동향
+          </h5>
+          <p className="mb-3 font-normal text-gray-700">
+            최신 기술 동향에 대한 기사를 통해 혁신적인 아이디어와 트렌드를
+            탐구해보세요. 이 글에서는 인공지능과 머신러닝의 발전이 우리 생활에
+            미치는 영향을 다룹니다.
+          </p>
+          <button className="rounded-lg bg-blue-700 px-3 py-2 text-sm font-medium text-white">
+            더보기
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
