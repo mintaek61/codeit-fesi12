37dfe9919d0581ce592db27cf02f74b7c760e7c0
mintaek <omt610@gmail.com>
2025-11-11 09:44:12 +0900

    기본 css 애니메이션 실습: 버튼 hover 시 배경색 변경 애니메이션

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="card-container">
-      <div className="card">영화 카드1</div>
-      <div className="card">영화 카드2</div>
-      <div className="card">영화 카드3</div>
-      <div className="card">영화 카드4</div>
-      <div className="card">영화 카드5</div>
+    <div className="container">
+      <button
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        className="btn"
+      >
+        아이콘 버튼
+      </button>
+      <div className={`menu-container ${isOpen ? "active" : ""}`}>
+        <div className="menu-item">메뉴1</div>
+        <div className="menu-item">메뉴2</div>
+        <div className="menu-item">메뉴3</div>
+      </div>
     </div>
   );
 }
